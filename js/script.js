491de6303073a97a3c796cb131ee8401f1fd3be9
Valentyn <valentunpron@gmail.com>
2022-06-12 19:26:04 +0300

    work js

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const headerList = document.querySelectorAll('.header__list'),
+	  catalogList = document.querySelector('.header__catalog'),
+	  catalogMenu = document.querySelector('.catalog__menu ')
+	  catalogLink = catalogList.querySelector('.catalog__link');
+	  
