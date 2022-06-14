693400a7f222e7333037cf16fa68b4bec6043523
Valentyn <valentunpron@gmail.com>
2022-06-14 10:10:58 +0300

    work this js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,25 @@
-const headerList = document.querySelectorAll('.header__list'),
-	  catalogList = document.querySelector('.header__catalog'),
-	  catalogMenu = document.querySelector('.catalog__menu ')
-	  catalogLink = catalogList.querySelector('.catalog__link');
-	  
+window.addEventListener('DOMContentLoaded', () => {
+	const headerList = document.querySelectorAll('.header__list'),
+		catalogList = document.querySelector('.header__catalog'),
+		catalogMenu = document.querySelector('.catalog__menu '),
+		catalogLink = catalogList.querySelector('.catalog__link');
+
+	function showBlock(item) {
+		item.classList.remove('none');
+		item.classList.add('active');
+	}
+
+	function hideBlock(item) {
+		item.classList.remove('active');
+		item.classList.add('none');
+	}
+	catalogLink.addEventListener('click', (e) => {
+		e.preventDefault();
+		if(catalogMenu.classList[1] == 'none') {
+			console.log(catalogMenu.classList[1])
+			showBlock(catalogMenu);
+		} else {
+			hideBlock(catalogMenu);
+		}
+	})
+})
