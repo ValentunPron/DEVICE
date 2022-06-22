ee75f9d744f7866135977b9d5dae32ad3ccfb804
Valentyn <valentunpron@gmail.com>
2022-06-22 13:43:54 +0300

    compelte work is info js

--- js/script.js
+++ js/script.js
@@ -4,15 +4,38 @@ window.addEventListener('DOMContentLoaded', () => {
 		catalogMenu = document.querySelector('.catalog__menu '),
 		catalogLink = catalogList.querySelector('.catalog__link'),
 		slideMenu = document.querySelectorAll('.slides section'),
-		slideButton = document.querySelectorAll('.description__number');
+		slideButton = document.querySelectorAll('.description__number'),
+		tabsParrent = document.querySelector('.info .container'),
+		tabs = tabsParrent.querySelectorAll('.info__row'),
+		tabsButton = document.querySelectorAll('.info__buttons .info_button');
+
+
+	// tabs
+
+	function hideTabsContent() {
+		tabs.forEach((item) => {
+			item.classList.add('none');
+		})
+	}
+
+	function showTabsContent(i = 0) {
+		tabs[i].classList.remove('none')
+	}
+
+	hideTabsContent();
+	showTabsContent();
+
+	tabsButton.forEach((item, i) => {
+		item.addEventListener('click', event => {
+			hideTabsContent();
+			showTabsContent(slideNumber(i));
+		})
+	})
+	// slide
 
 	slideButton.forEach((i, num) => i.addEventListener('click', (e) => {
-		slideButton.forEach(temp => {
-			temp.classList.remove('description__active');
-		})
-		i.classList.add('description__active');
-		console.log(num);
-		showSlideContent(slideNumber(num))
+		hideSlideContent();
+		showSlideContent(slideNumber(num), i);
 	}))
 
 	function slideNumber(number) {
@@ -24,9 +47,16 @@ window.addEventListener('DOMContentLoaded', () => {
 			return number = 2;
 		}
 	}
-	function showSlideContent(i = 0) {
+
+	function hideSlideContent() {
+		slideButton.forEach(temp => {
+			temp.classList.remove('description__active');
+		})
+	}
+	function showSlideContent(i = 0, item) {
 		slideMenu.forEach(temp => temp.classList.add('none'))
 		slideMenu[i].classList.remove('none');
+		item.classList.add('description__active');
 	}
 	//catalogButton
 	function showBlock(item) {
