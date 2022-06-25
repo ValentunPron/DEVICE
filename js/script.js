06fe41b45ea899a4e2ec8ca31d4e0ae3e2773401
Valentyn <valentunpron@gmail.com>
2022-06-25 16:10:45 +0300

    work complete is Opening Window

--- js/script.js
+++ js/script.js
@@ -162,10 +162,39 @@ window.addEventListener('DOMContentLoaded', () => {
 	}
 
 	mapButton.forEach(i => {
-		console.log(i);
 		i.addEventListener('click', event => {
 			event.preventDefault();
 			activeMap();
 		})
+	})
+
+	// Click Window
+	const writeWindow = document.querySelector('.write_us'),
+		  writeOpen = document.querySelector('.write_us__button'),
+		  writeExit = document.querySelector('.write_us__exit');
+
+	function openWindow() {
+		writeWindow.classList.remove('none');
+	}
+
+	function closeWindow() {
+		writeWindow.classList.add('none');
+	}
+	
+	writeOpen.addEventListener('click', (event) => {
+		openWindow();
+		writeExit.addEventListener('click', closeWindow);
+
+		document.addEventListener('keydown', (e) => {
+			if (e.code === 'Escape' && writeExit.style.display != 'none') {
+				closeWindow();
+			}
+		});
+
+		writeWindow.addEventListener('click', (event) => {
+			if (event.target === writeWindow ) {
+				closeWindow();
+			}
+		});
 	})
 })
